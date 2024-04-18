6b31fa786d4effa81d0160b5dd52b74e45d1d784
Marina Ivanova <marina.ivanova.ca@gmail.com>
2024-04-17 22:28:45 -0700

    ProductDisplay

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrums';
+import { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+	const { all_product } = useContext(ShopContext);
+	const { productId } = useParams();
+	const product =all_product.find((e)=>e.id === Number(productId))
 	return (
 		<div>
-			
+			<Breadcrum product={product} />
+			<ProductDisplay product={product} />
 		</div>
 	);
 }
