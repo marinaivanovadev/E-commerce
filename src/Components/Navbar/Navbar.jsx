7b9eeb31e06096d67fda5143695db615ad333958
Marina Ivanova <marina.ivanova.ca@gmail.com>
2024-04-23 15:19:19 -0700

    Mobile adaptation Home screen

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import './Navbar.css';
+import nav_dropdown from '../Assets/nav_dropdown.png' 
 
 import logo from "../Assets/logo.png";
 import cart from "../Assets/cart_icon.png";
@@ -10,6 +11,13 @@ const Navbar = () => {
 
 	const [menu, setMenu] = useState("shop");
 	const { getTotalCartItems } = useContext(ShopContext);
+	const menuRef = useRef();
+
+	const dropdown_toggle = (e) => {
+		menuRef.current.classList.toggle('nav-menu-visible');
+		e.target.classList.toggle('open');
+
+	}
 
 return (
 	<div className="navbar">
@@ -17,7 +25,8 @@ return (
 			<img src={logo} alt=""/>
 			<p>SHOPPER</p>
 		</div>
-		<ul className="nav-menu">
+		<img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+		<ul ref={menuRef} className="nav-menu">
 			<li onClick={() => { setMenu("shop") }}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{ menu ==="shop"?<hr/>:<></>}</li>
 			<li onClick={() => { setMenu("mens") }}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu === "mens" ? <hr /> : <></>}</li>
 			<li onClick={() => { setMenu("womens") }}><Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link>{menu === "womens" ? <hr /> : <></>}</li>
